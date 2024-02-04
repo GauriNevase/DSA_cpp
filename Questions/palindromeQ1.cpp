529a483e29ad950657d529235370f2c79039673e
GauriNevase <110117024+GauriNevase@users.noreply.github.com>
2024-02-04 21:53:55 +0530

    optimal solution

--- Questions/palindromeQ1.cpp
+++ Questions/palindromeQ1.cpp
@@ -33,6 +33,7 @@ bool isValid(char ch){
 	}
 
 }
+/*
 bool checkPalindrome(string s)
 {
     // Write your code here.
@@ -64,10 +65,47 @@ bool checkPalindrome(string s)
 	return 1;
 }
 
+*/
+//more optimal solution
+
+bool checkPalindrome(string st){
+        int s=0;
+        int e=st.length()-1;
+        while(s<=e){
+            if(st[s]!=st[e]){
+                return false;
+            }
+            else{
+                s++;
+                e--;
+            }
+        }
+        return true;
+        
+
+    }
+
+bool isPalindrome(string s) {
+        string temp="";
+        for(int i=0;i<s.length();i++){
+            if(isValid(s[i])){
+                temp.push_back(s[i]);
+            }
+        }
+
+        for(int i=0;i<temp.length();i++){
+            temp[i]=toLowerCase(temp[i]);
+        }
+
+        
+        return checkPalindrome(temp);
+    }
+
 
 int main(){
 	string s;
 	cin>>s;
-	cout<<"Palindrome or not ?"<<checkPalindrome(s)<<endl;
+	// cout<<"Palindrome or not ?"<<checkPalindrome(s)<<endl;
+	cout<<"Palindrome or not ?"<<isPalindrome(s)<<endl;
 
 }
